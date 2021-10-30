02f5bf217ee596ef1447f11eaa961d8ccb9ee3af
Andre Souza <ajjunior33@pop-os.localdomain>
2021-10-30 19:11:15 -0300

    Config font Inter.

--- App.js
+++ App.js
@@ -3,8 +3,16 @@ import {View, Text, StyleSheet} from 'react-native';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello,world</Text>
-      <Text style={styles.text}>Hi, André</Text>
+      <Text style={styles.title}>Lorem ipsum</Text>
+      <Text style={styles.text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex temporibus,
+        minus ratione delectus dolore incidunt voluptas accusamus facilis vitae
+        quo est nesciunt atque similique hic eius, sint, recusandae animi quam!
+      </Text>
+      <View style={styles.row}>
+        <Text style={styles.noFont}>André Souza</Text>
+        <Text style={styles.noFont}>21/10/2021</Text>
+      </View>
     </View>
   );
 };
@@ -14,17 +22,31 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    paddingHorizontal: 10,
   },
   title: {
     color: '#222',
-    fontWeight: 'bold',
     fontSize: 24,
-    fontFamily: 'Inter',
+    fontFamily: 'Inter Black',
   },
   text: {
     color: '#666',
-    fontSize: 16,
-    fontWeight: '400',
+    fontSize: 18,
+    fontFamily: 'Inter Medium',
+    textAlign: 'justify',
+    marginVertical: 30,
+  },
+  noFont: {
+    color: '#666',
+    fontSize: 18,
+    fontFamily: 'Inter',
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
   },
 });
 
